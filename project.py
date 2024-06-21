bdcbdfd05046d1f17d36240ccafcd92a8149242d
Shamushree <164496240+Shamushree@users.noreply.github.com>
2024-06-21 15:59:36 +0530

    Create project.py

---
+++ project.py
@@ -0,0 +1,6 @@
+def personal_details ():
+    name ,age = "jayashree" , 20
+    address = "no 133 t k street "
+    print("Name: {}\nage: {}\naddress: {}".format (name, age , address))
+    
+    personal_details()  
